7e4c3204b2790428ac96c47094dbfbe500f2c879
afux <afrank.mmt-m2014@fh-salzburg.ac.at>
2018-04-06 11:46:21 +0200

    stylus integrated

--- my-angular-app/gulpfile.js
+++ my-angular-app/gulpfile.js
@@ -1,5 +1,41 @@
 var gulp = require('gulp');
+//var data = require('gulp-data');
+var stylus = require('gulp-stylus');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Get one .styl file and render
+gulp.task('one', function () {
+  return gulp.src('./css/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css/build'));
 });
+
+// Options
+// Options compress
+gulp.task('compress', function () {
+  return gulp.src('./css/compressed.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('./css/build'));
+});
+
+
+// Set linenos
+gulp.task('linenos', function () {
+  return gulp.src('./css/linenos.styl')
+    .pipe(stylus({linenos: true}))
+    .pipe(gulp.dest('./css/build'));
+});
+
+// Include css
+// Stylus has an awkward and perplexing 'include css' option
+gulp.task('include-css', function() {
+  return gulp.src('./css/*.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(gulp.dest('./'));
+
+});
+
+gulp.task('default',['one', 'compress', 'linenos']);
